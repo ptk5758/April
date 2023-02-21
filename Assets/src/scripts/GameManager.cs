686a1569c823a9b5c3a1104ffd2d47b4c520faaa
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-22 01:12:11 +0900

    add gamemager script

---
+++ Assets/src/scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (instance != null || instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
--- Assets/src/scripts/item/Item.cs
+++ Assets/src/scripts/item/Item.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Item : MonoBehaviour
-{    
+{
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag != "Player") return;
