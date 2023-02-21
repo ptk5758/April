b1b34d55aa1be1396e0450137f9c7442574e8a20
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-21 19:51:29 +0900

    rigid & moving func

--- Assets/src/scripts/rabbit/Rabbit.cs
+++ Assets/src/scripts/rabbit/Rabbit.cs
@@ -12,8 +12,8 @@ public abstract class Rabbit : MonoBehaviour
 
     private void Moving()
     {
-        float h = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        float v = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        transform.Translate(new Vector3(h, 0, v));
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(h, 0, v).normalized * Time.deltaTime * speed;
     }
 }
