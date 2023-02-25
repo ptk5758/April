d92abf3860bdd5f7a5c476fe3a97524a1c4aa205
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-25 19:44:50 +0900

    egg drop func

---
+++ Assets/src/scripts/Nest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nest : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag != "Player") return;
+        Rabbit rabbit = other.gameObject.GetComponent<Rabbit>();
+        int count = rabbit.eggs.Count;
+        rabbit.eggs.Clear();
+    }
+
+}
