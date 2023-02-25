e32da93de0fb0520ca385737f1f87b087c562957
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-25 20:52:59 +0900

    enemy nav mesh add

--- Assets/src/scripts/Enemy/Enemy.cs
+++ Assets/src/scripts/Enemy/Enemy.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class Enemy : MonoBehaviour, IEnemy
-{    
-    
+{
+    public Transform target;
+    public NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();        
+    }
+
+    private void FixedUpdate()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
 }
