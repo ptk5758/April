925f3f2800ee5acfc654b6c5a3fc2fe25cf1d6de
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-03-11 21:13:01 +0900

    Pick Up Egg Function

--- Assets/src/scripts/rabbit/Rabbit.cs
+++ Assets/src/scripts/rabbit/Rabbit.cs
@@ -25,11 +25,13 @@ public abstract class Rabbit : MonoBehaviour, IRabbit
     [Header("Attribute Variable")]
     public float speed = 1; // 토끼의 속도
 
+
     [field:SerializeField]
     public Item NearItem { get; set; } // 토끼가 가지고 있는함수
     private Item lastNearItem = null; // 토끼가 마지막으로 가지고 있던함수
     [field:SerializeField]
     public Item Item { get; set; } // 실제로 가지고있는 아이템
+    public List<Egg> carryEgg;
 
 
     private void Awake()
@@ -43,6 +45,7 @@ public abstract class Rabbit : MonoBehaviour, IRabbit
             instance = this;
         }
         gameManager = GameManager.Instance;
+        carryEgg = new List<Egg>();
         DontDestroyOnLoad(gameObject);
     }
     private void Update()
@@ -71,9 +74,20 @@ public abstract class Rabbit : MonoBehaviour, IRabbit
         transform.position += new Vector3(h, 0, v).normalized * Time.deltaTime * speed;
     }
 
-    public void PickUpItem()
+    public void PickUpItem() // 아이템을 줍는함수
     {
-        Debug.Log("TTT");
+        if (NearItem == null) return;
+        if (NearItem.type == Item.Type.EGG)
+        {
+            PickUpEgg(NearItem);
+        }
+    }
+
+    private void PickUpEgg(Item item)
+    {
+        carryEgg.Add((Egg) item);
+        NearItem = null;
+        item.gameObject.SetActive(false);
     }
 
     public void OnHit(Enemy enemy)
