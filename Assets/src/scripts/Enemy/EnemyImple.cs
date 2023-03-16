248e6a153cdcdb08c6125626fcb3b784838618a6
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-03-16 15:04:25 +0900

    enemy moving control

--- Assets/src/scripts/Enemy/EnemyImple.cs
+++ Assets/src/scripts/Enemy/EnemyImple.cs
@@ -5,30 +5,52 @@ using UnityEngine.AI;
 
 public abstract class EnemyImple : MonoBehaviour, Enemy
 {
+    public enum Status { 
+        MOVE,
+        STOP
+    }
+
     private Transform target;
     public NavMeshAgent navMeshAgent;
     private Rabbit rabbit;
+
+    public float speed = 1;
+    public bool isMove;
+    public Status status;
 
     private void Awake()
     {
         rabbit = Rabbit.Instance;
         target = rabbit.transform;
-        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();        
     }
 
-    private void FixedUpdate()
+    private void Update()
+    {        
+        SetMoving(isMove);
+        if (status == Status.MOVE)
+            navMeshAgent.SetDestination(target.position);
+    }
+
+    private void SetMoving(bool value)
     {
-        navMeshAgent.SetDestination(target.position);
+        if (value)
+        {
+            navMeshAgent.speed = this.speed;
+            this.status = Status.MOVE;
+        }
+        else
+        {
+            navMeshAgent.speed = 0;
+            this.status = Status.STOP;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
-    {
-        
+    {        
         if (collision.gameObject.tag != "Player") return;
         Rabbit rabbit = collision.gameObject.GetComponent<Rabbit>(); // find Rabbit instance
         rabbit.DropEgg(); // Rabbit Carry Egg Clear
         rabbit.OnHit(this); // Rabbit OnHit Call
-        // collision.gameObject.GetComponent<Rabbit>().OnHit(this);
-
     }
 }
