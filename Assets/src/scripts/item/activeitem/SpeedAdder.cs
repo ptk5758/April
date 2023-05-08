1cfb2840e9abb8d8f7dfaea06d155b98b0f94474
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-05-09 02:14:32 +0900

    코루틴 같은 경우 오브젝트가 비활성화 되면 중단 됌

--- Assets/src/scripts/item/activeitem/SpeedAdder.cs
+++ Assets/src/scripts/item/activeitem/SpeedAdder.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class SpeedAdder : ActiveItem
 {
+    Rabbit rabbit;
     public override void UseItem()
     {
-        Debug.Log("¼Óµµ°¡ »¡¶óÁü;;");
+        // Debug.Log("¼Óµµ°¡ »¡¶óÁü;;");
+        rabbit = Rabbit.Instance;
+        // StartCoroutine(SpeedUp());
     }
+    /*
+    IEnumerator SpeedUp()
+    {
+        rabbit.speed += 10;
+        yield return new WaitForSeconds(5f);
+        rabbit.speed -= 10;
+    }
+    */
+
 }
