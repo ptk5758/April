06c2c5450ddbcd71864ca9b3cdc848e4ed481616
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-07-07 12:58:49 +0900

    Enemey Set Target Function

--- Assets/src/scripts/enemy/Enemy.cs
+++ Assets/src/scripts/enemy/Enemy.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class Enemy : MonoBehaviour
 {
     DetectObserver detectObserver;
+    Destination destination;
     private void Awake()
     {
         this.detectObserver = new DetectObserver();
+        this.destination = new Destination(gameObject.GetComponent<NavMeshAgent>());
     }
     private void OnCollisionEnter(Collision collision)
     {
         detectObserver.DetectedObject(collision.gameObject);
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
+class Destination
+{
+    NavMeshAgent navMeshAgent;
+    public Destination(NavMeshAgent navMeshAgent)
+    {
+        this.navMeshAgent = navMeshAgent;
+    }
+
+    public void SetTarget(Transform transform)
+    {
+        this.navMeshAgent.SetDestination(transform.position);
     }
 }
 
