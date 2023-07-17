8645e989d94bc4fc6d566dedaf4373de6c5c2f16
ptk5758 <ptk725739@gmail.com>
2023-07-17 13:38:29 +0900

    item timer script add

---
+++ Assets/src/scripts/item/activeitem/Timer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : ActiveItem
+{
+    public int time;
+    public override void UseItem()
+    {
+        GameManager.Instance.playTime += time;
+    }
+}
