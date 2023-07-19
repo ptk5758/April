806a08f7398e7619838ee780c987f83dcfc78c82
ji0ne <zi0neeeeeeeee@gmail.com>
2023-07-20 00:53:04 +0900

    add Logline

---
+++ Assets/src/scripts/LogLine/Logline.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logline : MonoBehaviour
+{
+    public GameObject[] logImage;
+    public int images = 0;
+
+
+    void Update()
+    {
+        //여기서 기본적으로 5초 지나면 옵젝 활성화되도록
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            try
+            {
+                GameObject logImages = logImage[images];
+
+                images++;
+
+                logImages.SetActive(true);
+
+            }
+            catch
+            {
+                GoTitle();
+            }
+
+        }
+    }
+
+    void GoTitle()
+    {
+        Debug.Log("title로 이동");
+    }
+}
---
+++ Assets/src/scripts/LogLine/SkipLogline.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipLogline : MonoBehaviour
+{
+    //이후 씬 연결 시 스킵 > title로 전환
+    public void SkipStory()
+    {
+        Debug.Log("로그라인 skip : title로 바로가기 ");
+    }
+    
+
+}
