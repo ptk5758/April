752e31a0ece185ae332a62ffd04c4b7927b58bee
ji0ne <zi0neeeeeeeee@gmail.com>
2023-07-29 15:52:03 +0900

    0729 pull받기용 커밋

--- Assets/src/scripts/GameManager.cs
+++ Assets/src/scripts/GameManager.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -71,4 +72,82 @@ public class GameManager : MonoBehaviour
         uiManager.CloseEndingBoard();
     }
     
+=======
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance 
+    {
+        get
+        {
+            if (instance == null) 
+            {
+                var obj = FindObjectOfType<GameManager>();
+                if (obj != null)
+                    instance = obj;                
+            }
+            return instance;
+
+        }
+        private set { instance = value; }
+    }
+
+    public static float playTime = 300f;
+    public static int eggCount = 0;
+
+
+    [SerializeField]
+    EnemyController enemyController;
+
+    [SerializeField]
+    UIManager uiManager;
+
+    private void Update()
+    {
+        playTime -= Time.deltaTime;             
+    }
+    private void Start()
+    {
+        StartCoroutine(enemyController.SummonCoroutine()); // 여우 소환 코루틴 시작!
+    }
+
+    private void LateUpdate()
+    {
+        
+        uiManager.Update();
+    }
+
+    private void Awake()
+    {        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }        
+        DontDestroyOnLoad(gameObject);     
+    }
+
+    public void GameEnd()
+    {
+        uiManager.OpenEndingBoard();
+    }
+
+    public void CloseEndingBoard()
+    {
+        uiManager.CloseEndingBoard();
+    }
+    
+<<<<<<< HEAD
+>>>>>>> 8ceefe74aaab2abab0bcd5e5f590c649833beca5
+=======
+>>>>>>> 8ceefe74aaab2abab0bcd5e5f590c649833beca5
 }
