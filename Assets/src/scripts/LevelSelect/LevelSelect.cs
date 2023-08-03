015cc120b92b1a1cb9ce983b6ea5c145d3cba2c9
asy <asy951013@naver.com>
2023-08-03 22:08:03 +0900

    SelectLevel 추가

--- Assets/src/scripts/LevelSelect/LevelSelect.cs
+++ Assets/src/scripts/LevelSelect/LevelSelect.cs
@@ -14,7 +14,6 @@ public class LevelSelect : MonoBehaviour
         LevelNumber.GetInt(i);
         Debug.Log("Level0 버튼 선택");
         PlayerPrefs.SetInt("GameLevel", (int)value);
-        //Level.NORMAL의 int 값은 0이니까 0을 PlayerPrefs가 가지고 있음
         SceneManager.LoadScene("MapItemSelect");
         //맵 Scene으로 바뀌는 코드
     }
