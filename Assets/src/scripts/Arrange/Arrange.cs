3f921f79fe5137a074f82c4cdb45987f6fb48d3f
asy <asy951013@naver.com>
2023-08-03 21:07:56 +0900

    PlayerPrefs 추가

--- Assets/src/scripts/Arrange/Arrange.cs
+++ Assets/src/scripts/Arrange/Arrange.cs
@@ -14,6 +14,7 @@ public class Arrange : MonoBehaviour
     void Awake()
     {
         RandomArrange();
+        Debug.Log(PlayerPrefs.GetInt("GameLevel"));
     }
 
     public void RandomArrange()
